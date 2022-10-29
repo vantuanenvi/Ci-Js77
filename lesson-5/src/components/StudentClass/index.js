5b8b77a6ab7c2fc8deb2b225b51e1d26cb0c0884
vantuanenvi <tranvantuanen@gmail.com>
2022-10-29 15:40:51 +0700

    lesson-5-ex2

--- lesson-5/src/components/StudentClass/index.js
+++ lesson-5/src/components/StudentClass/index.js
@@ -1,33 +1,32 @@
 import React from "react";
-import{useState} from 'react'
-class StudentClass extends React.Component{
-    constructor(props){
+import { useState } from "react";
+class StudentClass extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
-        students : {
-            name: 'Mindxer',
-            age: 8,
-        },    
-    };     
+      students: {
+        name: "Mindxer",
+        age: 8,
+      },
+    };
+  }
+  ClickIncreaseAge = (state) => {
+    this.setState((state) => ({
+      students: { age: this.state.students.age + 1 },
+    }));
+  };
+  render() {
+    return (
+      <div>
+        <p>
+          Hello I am {this.state.students.name} and i am{" "}
+          {this.state.students.age} years old
+        </p>
+        <button onClick={this.ClickIncreaseAge}>
+          Click to Increase age of me
+        </button>
+      </div>
+    );
+  }
 }
-    ClickIncreaseAge = (prev)=>
-    {
-     function setStudent(prev){
-        console.log(prev)
-            return {... prev, age: prev.age + 1  }
-        }
-        setStudent(this.state.students)
-    }
-  
-
-    render(){
-        return(
-            <div>
-                <p> Hello I am {this.state.students.name} and i am {this.state.students.age} years old</p>
-                <button onClick={this.ClickIncreaseAge}>
-                Click to Increase age of me </button>
-            </div>
-        )
-    }
-    }
-export default StudentClass
+export default StudentClass;
