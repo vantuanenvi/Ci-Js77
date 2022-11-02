ad81866335d9183518159c2e0ac2c2e3e901ccc3
vantuanenvi <tranvantuanen@gmail.com>
2022-11-02 15:17:33 +0700

    test

--- lesson-4/src/components/Card/index.js
+++ lesson-4/src/components/Card/index.js
@@ -1,59 +1,92 @@
 import "./style.css";
-import React, { Component } from 'react'
-import podcasts from "../../data/podcasts"
-import {FaBookmark, FaPlay} from "react-icons/fa";
-import {BsThreeDots} from "react-icons/bs"
-function Card({}){
-    return(
-        <div className="card">
-        <div className="left">
-        {podcasts.slice(0,3).map((podcast) =>(
-        
-            <div className="item">
-                <div className="box">
-                <span className="rank">{podcast.id}</span>
-                <div className="play-btn">
-                    <button className="hidden"><FaPlay/></button>
-                    </div>
-            <img className="cover" src= {podcast.cover}></img>
-            <div className="content">
+import React, { Component } from "react";
+import podcasts from "../../data/podcasts";
+import { FaBookmark, FaPlay } from "react-icons/fa";
+import { BsThreeDots } from "react-icons/bs";
+function Card({}) {
+  return (
+    <div className="card">
+      <div className="left">
+
+        {podcasts.slice(0, 3).map((podcast) => (
+          <div className="item">
+            <div className="box">
+              <span className="rank">{podcast.id}</span>
+              <div className="play-btn">
+                <button className="hidden">
+                  <FaPlay />
+                </button>
+              </div>
+              <img className="cover" src={podcast.cover}></img>
+      <div className="sum">
+
+              <div className="content">
                 <div className="name">{podcast.name}</div>
                 <h3 className="chanel">{podcast.chanel}</h3>
-                <div className="icon-date">
-                <div className="date"><h3 className="date">{podcast.date}</h3></div>
-           <div className="right-item"> <FaBookmark className="icon"/> < BsThreeDots className="icon"/>
-           
-           </div>
+             
+                  <div className="date">
+                    <span className="date">{podcast.date}</span>
+                    <span className="update">{podcast.update}</span>
+                  </div>
+                  </div>
+              </div>
+                  <div className="mark-more">
+                  <span className="right-item">
+                    {" "}
+                    <FaBookmark className="icon" />{" "}
+                    <BsThreeDots className="icon" />
+                  </span>
+                  </div>
+                  </div>
+
+              
             </div>
-                </div>
-            </div>
-            </div>
-        )
-        )}
-         </div>
-        <div className="right">
-        {podcasts.slice(3,6).map((podcast) =>(
-            <div className="item">
-                <div className="box">
-                <span className="rank">{podcast.id}</span>
-                <div className="play-btn">
-                    <button className="hidden"><FaPlay/></button>
-                    </div>
-            <img className="cover" src= {podcast.cover}></img>
-            <div className="content">
+        ))}
+      </div>
+   
+
+      <div className="right">
+
+        {podcasts.slice(3, 6).map((podcast) => (
+
+          <div className="item">
+            <div className="box">
+              <span className="rank">{podcast.id}</span>
+              <div className="play-btn">
+                <button className="hidden">
+                  <FaPlay />
+                </button>
+              </div>
+              <img className="cover" src={podcast.cover}></img>
+        <div className="sum">
+
+              <div className="content">
                 <div className="name">{podcast.name}</div>
                 <div className="chanel">{podcast.chanel}</div>
-                <div className="date">{podcast.date}</div>
+                <div className="date">
+                  <span className="date">
+                    {podcast.date}
+                  </span>
+                  <span className="update">{podcast.update}</span>
+                  </div>
+                  </div>
+              </div>
             </div>
-                </div>
-            </div>
-        )
-        
-        )}
-        </div>
-       
-        </div>
-    
-    )
+
+                  <div className="mark-more">
+                  <span className="right-item">
+                    {" "}
+                    <FaBookmark className="icon" />{" "}
+                    <BsThreeDots className="icon" />
+                  </span>
+                  </div>
+           
+    </div>
+
+        ))}
+      </div>
+    </div>
+
+  );
 }
-export default Card
+export default Card;
