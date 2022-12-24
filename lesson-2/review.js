a9e2041b04b89223b20177ed60f44a924b7194b7
vantuanenvi <tranvantuanen@gmail.com>
2022-12-24 20:54:18 +0700

    update-question

---
+++ lesson-2/review.js
@@ -0,0 +1,58 @@
+/*
+    Scope:
+    - global
+    - function
+    -block scope:
+    {
+
+    }
+
+    1. Sự khác biệt giữa let, const và var:
+
+    * Scope(phạm vi)
+    - var -> global
+    const, let : function, block 
+    
+    * Assignment:
+    - const -> không thể re-assignment
+    let và var là có thể
+
+    * Hoisting
+    - var có thể sử dụng trước khi khai báo -> undefined
+    - let và const -> báo lỗi
+
+    2. ES6
+    - map 
+    - filter
+    - reduce
+    - some
+    - every
+    - find
+    - findIndex
+    - for with index (for (let i = 0; i < num.length; i++))
+    - for in
+    - for of
+    - for each 
+
+    3. Spread operator(...)
+
+    4. String template (literals string)
+
+    5. Arrow function:
+        - Có bao nhiêu function
+        - Sự khác biệt của arrow function
+        - Tại sao Es6 lại tạo ra arrow function
+        const getSum = (nums) => {} ( bind, call, this)
+    
+    6. Destructuring:
+        - Object Destructuring
+        - Array Destructuring
+
+    7. Promise:
+        - Cách tạo promise
+        -  Mỗi promise có bao nhiêu state
+        - Cách handle promise => async/ await, event-loop
+
+    8. Class:
+    
+    */
